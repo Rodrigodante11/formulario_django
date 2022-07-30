0101c0951da65ed9d7cbe62ac7c993c37e38db83
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2022-07-30 20:58:24 -0300

    widget e calendario do django

--- passagens/forms.py
+++ passagens/forms.py
@@ -1,8 +1,15 @@
 from django import forms
+from tempus_dominus.widgets import DatePicker
 
 
 class PassagemForms(forms.Form):
     origem = forms.CharField(label='Origem', max_length=100)
     destino = forms.CharField(label='Destino', max_length=100)
-    data_ida = forms.DateField(label='Ida')
-    data_volta = forms.DateField(label='Volta')
+
+    data_ida = forms.DateField(label='Ida', widget=DatePicker(
+        options={
+            'minDate': '2009-01-20',
+            'maxDate': '2023-01-20',
+        },
+    ),)
+    data_volta = forms.DateField(label='Volta', widget=DatePicker())
