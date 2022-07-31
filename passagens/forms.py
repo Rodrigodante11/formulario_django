3607df2d11406efbd9e8a49b331b7235031202c5
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2022-07-30 21:32:03 -0300

    adicionando o widget tweaks do django

--- passagens/forms.py
+++ passagens/forms.py
@@ -3,23 +3,24 @@ from tempus_dominus.widgets import DatePicker
 from datetime import datetime
 from passagens.classe_viagem import tipos_de_classe
 
-class PassagemForms(forms.Form):
-    origem = forms.CharField(label='Origem: ', max_length=100)
-    destino = forms.CharField(label='Destino: ', max_length=100)
 
-    data_ida = forms.DateField(label='Ida', widget=DatePicker(
+class PassagemForms(forms.Form):
+    origem = forms.CharField(label='Origem:', max_length=100)
+    destino = forms.CharField(label='Destino:', max_length=100)
+
+    data_ida = forms.DateField(label='Ida:', widget=DatePicker(
         options={
             'minDate': str(datetime.today()),
             'maxDate': '2023-01-20',
         },
     ),)
-    data_volta = forms.DateField(label='Volta: ', widget=DatePicker())
-    data_pesquisa = forms.DateField(label='Data da pesquisa: ', disabled=True, initial=datetime.today())
-    classe_viagem = forms.ChoiceField(label='Classe do Voo: ', choices=tipos_de_classe)
+    data_volta = forms.DateField(label='Volta:', widget=DatePicker())
+    data_pesquisa = forms.DateField(label='Data da pesquisa:', disabled=True, initial=datetime.today())
+    classe_viagem = forms.ChoiceField(label='Classe do Voo:', choices=tipos_de_classe)
     informacoes = forms.CharField(
-        label='Informacoes extrax: ',
+        label='Informacoes extrax:',
         max_length=200,
         widget=forms.Textarea(),
         required=False
     )
-    email = forms.EmailField(label='Email: ', max_length=150)
+    email = forms.EmailField(label='Email:', max_length=150)
