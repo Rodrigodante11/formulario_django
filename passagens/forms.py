903c54a9baf55b376b1c7f01f0dcc69a52998e07
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2022-07-30 22:15:37 -0300

    validando as datas

--- passagens/forms.py
+++ passagens/forms.py
@@ -28,15 +28,18 @@ class PassagemForms(forms.Form):
 
     def clean(self):
         origem = self.cleaned_data.get('origem')
+        data_ida = self.cleaned_data.get('data_ida')
+        data_volta = self.cleaned_data.get('data_volta')
         destino = self.cleaned_data.get('destino')
         lista_erros = {}
 
         campo_tem_algum_numero(origem, 'origem',  lista_erros)
         campo_tem_algum_numero(destino, 'destino', lista_erros)
         origem_destino_iguais(origem, destino, lista_erros)
+        data_id_maior_que_data_volta(data_ida, data_volta, lista_erros)
 
         if lista_erros is not None:  # Verifica se tem algum erro
             for erro in lista_erros:  # Para cada erro
                 mensagem_erro = lista_erros[erro]  # pegando a mensagem do erro
-                self.add_error(erro, mensagem_erro)  
+                self.add_error(erro, mensagem_erro)
         return self.cleaned_data
--- passagens/validation.py
+++ passagens/validation.py
@@ -22,3 +22,17 @@ def campo_tem_algum_numero(valor_campo, nome_campo, lista_de_erros):
     """
     if any(char.isdigit() for char in valor_campo):  # Verificando se um dos caracteres eh digito
         lista_de_erros[nome_campo] = 'Nao inclua Numeros!'
+
+
+def data_id_maior_que_data_volta(data_ida, data_volta, lista_de_erros):
+    """
+    verifica se a data de eh maior que a data de volta
+
+    :param data_ida:
+    :param data_volta:
+    :param lista_de_erros:
+    :return:
+    """
+    if data_ida > data_volta:
+        assert isinstance(lista_de_erros, object)
+        lista_de_erros['data_volta'] = 'data de Volta nao pode ser menor que a data de Ida'
